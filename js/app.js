73e9f1e5723b8e586f2045973ca0afeca5982fcd
DanielMarcano <danielemarcano96@gmail.com>
2017-05-12 16:57:54 -0430

    Añadidas funciones llenarColumnas, chequearTablero y actualizarArraysCaramelos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-// Mis funciones
+// Arrays de caramelos
+var caramelosCol1,
+caramelosCol2,
+caramelosCol3,
+caramelosCol4,
+caramelosCol5,
+caramelosCol6,
+caramelosCol7;
 
-// Inicio de la funcion que prende y apaga el titulo Match Game
+
+// Funcion de la documentación de Mozilla para obtener números Random
+// El min es incluyente y el max excluyente
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/*Inicio de mis funciones*/
+
+//Hace que el título Match Game cambie de color infinitamente
 function prenderApagar(selector) {
   $(selector).animate({
-    opacity: '1'
+    opacity: '1',
   },
   {
     step: function() {
@@ -11,40 +29,106 @@ function prenderApagar(selector) {
     },
     queue: true
   }
-  )
-  .animate({
-    opacity: '1'
+)
+.animate({
+  opacity: '1'
+},
+{
+  step: function() {
+    $(this).css('color', '#DCFF0E')
   },
-  {
-    step: function() {
-      $(this).css('color', '#DCFF0E')
-    },
-    queue: true
-  }, 1000
-  )
-  .delay(1000)
-  .animate({
-    opacity: '1'
+  queue: true
+}, 1000
+)
+.delay(1000)
+.animate({
+  opacity: '1'
+},
+{
+  step: function() {
+    $(this).css('color', 'purple')
+    prenderApagar(selector)
   },
-  {
-    step: function() {
-      $(this).css('color', 'purple')
-      prenderApagar(selector)
-    },
-    queue: true
-  }
-  )
+  queue: true
 }
-// Fin de la funcion que prende y apaga el titulo Match Game
+)
+}
 
-// Fin de mis funciones
+// Con esta funcion, actualizamos los arrays de caramelos
+function actualizarArraysCaramelos(selector) {
+  $(selector).each(function(i) {
+    switch (i) {
+      case 0:
+        caramelosCol1 = $(this).children();
+        break;
+      case 1:
+        caramelosCol2 = $(this).children();
+        break;
+      case 2:
+        caramelosCol3 = $(this).children();
+        break;
+      case 3:
+        caramelosCol4 = $(this).children();
+        break;
+      case 4:
+        caramelosCol5 = $(this).children();
+        break;
+      case 5:
+        caramelosCol6 = $(this).children();
+        break;
+      case 6:
+        caramelosCol7 = $(this).children();
+        break;
+    }
+  });
+
+
+}
+
+//Se activa cada vez que se inicia el juego, u ocurren cambios en el tablero
+function chequearTablero() {
+  // Si hay columnas sin dulces, esta función las rellenará
+  llenarColumnas('[class^="col-"]');
+  actualizarArraysCaramelos('[class^="col-"]');
+  // Esta funcion es de prueba
+  validacionVertical();
+}
+
+function llenarColumnas(selector) {
+  var tope = 6;
+  var columna = $(selector);
+
+  $(selector).each(function() {
+    var caramelos = $(this).children().length;
+    var agrega = tope - caramelos;
+    for (var i = 0; i < agrega; i++) {
+      // Obtiene un caramelo al azar
+      var numeroCaramelo = getRandomInt(1, 5)
+      // Si la columna no tiene caramelos y estás en la primera iteración
+      // usa append
+      if (i == 0 && caramelos < 1) {
+        $(this).append('<img src="image/'+numeroCaramelo+'.png" style="display: none;" class="ui-state-default sortable"></img>');
+        $(this).find('img').slideDown(1000);
+      } else {
+        // Sino, usa before, para que los nuevos caramelos
+        // empujen a los viejos hacia abajo
+        $(this).find('img:eq(0)').before('<img src="image/'+numeroCaramelo+'.png" style="display: none;" class="ui-state-default sortable"></img>')
+        $(this).find('img:eq(0)').slideDown(1000);
+      }
+    }
+  });
+}
+
+/* Fin de mis funciones */
 
 // Inicio de la función $(document).ready
+var respuesta;
 $(function() {
   // Se activa infinitamente la animación del título
   // prenderApagar('h1.main-titulo');
-
-
+  $('.btn-reinicio').click(function() {
+    chequearTablero();
+  });
 
 });
 // Fin de la función $(document).ready
